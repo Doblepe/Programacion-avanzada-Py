2708704db8be392b5d568c211926e7dab481df1c
Doblepe <vicdobleperez@gmail.com>
2022-12-07 09:33:28 +0100

    Leccion1

---
+++ Leccion1.py
@@ -0,0 +1,8 @@
+# milista = [1,23,45,5,['m',4]]
+# print('Leccion 1')
+# print(milista[4][1])
+#Realiza una calculadora
+rango = range(11)
+rango = list(rango)  
+for num in rango:
+    print(f'{num} por {rango[num]} = {rango[num] * num}')
